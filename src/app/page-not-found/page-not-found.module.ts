0610c30093727710fbfd3322d5c6a65915f5ed9c
David An <dong82@gmail.com>
2021-03-02 20:47:00 -0500

    Tesk Token Auth

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PageNotFoundComponent]
+  declarations: [
+    PageNotFoundComponent
+  ]
 })
 export class PageNotFoundModule { }
